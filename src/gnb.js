56cc7e7b6bc452e21bf96c9263c3ddf21be29f51
gilmujjang <alsanrlf@naver.com>
2022-02-21 23:47:58 +0900

    feat: 헤더 모바일 버전 작성

--- src/gnb.js
+++ src/gnb.js
@@ -11,16 +11,16 @@ const initGNB = () => {
         <ul class="flexBox" style="justify-content: flex-end">
           <li><a class="topNavAnchor" href="/account/login/?next=/">로그인</a></li>
           <li>
-            <a class="topNavAnchor leftBar" href="#"
-              ><span>회원가입</span><span style="color: #46cfa7">(1000포인트 지급!)</span></a
-            >
+            <a class="topNavAnchor leftBar" href="#">
+              <span>회원가입</span>
+              <span style="color: #46cfa7">(1000포인트 지급!)</span>
+            </a>
           </li>
           <li><a class="topNavAnchor leftBar" href="/faq">꾸까 고객센터</a></li>
           <li>
             <a
               class="topNavAnchor leftBar"
-              href="https://partners.kukka.kr/?utm_source=kukka&utm_medium=affiliate&utm_campaign=kukka&utm_content=partners"
-            >
+              href="https://partners.kukka.kr/?utm_source=kukka&utm_medium=affiliate&utm_campaign=kukka&utm_content=partners">
               가입제휴
             </a>
           </li>
@@ -30,13 +30,11 @@ const initGNB = () => {
             <a href="/">
               <img
                 class="headerKukkaIcon"
-                src="../public/img/kukkaLogo.png"
-                alt="kukka 로고"
-                style="width: 167px; height: 22px"
-              />
+                src="../public/img/kukkaDeskTopLogo.png"
+                alt="kukka 로고"/>
             </a>
           </div>
-          <ul class="flexBox" style="margin: 0 24px; padding: 0">
+          <ul class="flexBox" style="margin:16px 36px; padding: 0">
             <li><a class="navAnchor headerTag" href="/subscription/">꽃 정기구독</a></li>
             <li><a class="navAnchor headerTag" href="/shop/flowers/">꽃다발</a></li>
             <li><a class="navAnchor headerTag" href="/shop/quick/">당일배송</a></li>
@@ -44,12 +42,12 @@ const initGNB = () => {
             <li><a class="navAnchor headerTag" href="/item/all/">소품샾</a></li>
             <li><a class="navAnchor headerTag" href="/event">이벤트</a></li>
           </ul>
-          <div>
+          <div style="margin-top: 8px">
             <a href="/">
               <img class="headerNavIcon" src="../public/img/people.png" alt="마이페이지 로고" />
             </a>
             <a href="/cart/?next=/" class="func cart">
-              <img class="headerNavIvon" src="../public/img/basket.png" alt="장바구니 아이콘" />
+              <img class="headerNavIcon" src="../public/img/basket.png" alt="장바구니 아이콘" />
             </a>
           </div>
         </div>
@@ -57,20 +55,20 @@ const initGNB = () => {
       `;
     }
     return `
-      <nav style="display: flex; justify-content: space-between; margin-top: 15px">
+      <nav class="flexBox spaceBetween" style="margin-top: 15px">
         <a href="/">
-          <img src="../public/img/kukkaLogo.png" alt="kukka 로고" style="width: 167px; height: 22px" />
+          <img class="headerKukkaIcon" src="../public/img/kukkaMobileLogo.png" alt="kukka 로고" />
         </a>
         <a href="/cart/?next=/" class="func cart">
-          <img src="../public/img/basket.png" alt="장바구니 아이콘" style="width: 21px; height: 23px" />
+          <img class="headerNavIvon" src="../public/img/basket.png" alt="장바구니 아이콘" />
         </a>
       </nav>
       <nav>
-        <ul style="list-style: none; margin-top: 15px; font-size: 11px; display: flex; justify-content: space-between">
-          <li><a>정기구독</a></li>
-          <li><a>꽃다발</a></li>
-          <li><a>당일배송</a></li>
-          <li><a>플라워클래스</a></li>
+        <ul class="mobileHeader">
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/subscription/">정기구독</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/shop/flower/">꽃다발</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/shop/quick/">당일배송</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/class/">플라워클래스</a></li>
         </ul>
       </nav>
     `;
