aac19599ace7edfa6cc2842577e996987dd16c86
gilmujjang <alsanrlf@naver.com>
2022-02-21 23:14:48 +0900

    feat: 데스크탑 화면 헤더 작성

--- src/gnb.js
+++ src/gnb.js
@@ -8,27 +8,50 @@ const initGNB = () => {
     if (matchMedia('screen and (min-width: 786px)').matches) {
       return `
       <nav>
-        <ul>
-          <li><a href="/account/login/?next=/">로그인</a></li>
-          <li><a href="#">회원가입</a></li>
-          <li><a href="/faq">꾸까 고객센터</a></li>
+        <ul class="flexBox" style="justify-content: flex-end">
+          <li><a class="topNavAnchor" href="/account/login/?next=/">로그인</a></li>
           <li>
-            <a href="https://partners.kukka.kr/?utm_source=kukka&utm_medium=affiliate&utm_campaign=kukka&utm_content=partners"
-              >가입제휴</a
+            <a class="topNavAnchor leftBar" href="#"
+              ><span>회원가입</span><span style="color: #46cfa7">(1000포인트 지급!)</span></a
             >
           </li>
+          <li><a class="topNavAnchor leftBar" href="/faq">꾸까 고객센터</a></li>
+          <li>
+            <a
+              class="topNavAnchor leftBar"
+              href="https://partners.kukka.kr/?utm_source=kukka&utm_medium=affiliate&utm_campaign=kukka&utm_content=partners"
+            >
+              가입제휴
+            </a>
+          </li>
         </ul>
-        <div>
-          <div>로고</div>
-          <ul>
-            <li><a href="/subscription/">꽃 정기구독</a></li>
-            <li><a href="/shop/flowers/">꽃다발</a></li>
-            <li><a href="/shop/quick/">당일배송</a></li>
-            <li><a href="/class/">플라워클래스</a></li>
-            <li><a href="/item/all/">소품샾</a></li>
-            <li><a href="/event">이벤트</a></li>
+        <div class="flexBox" style="justify-content: center; margin: 32px 16px 64px">
+          <div>
+            <a href="/">
+              <img
+                class="headerKukkaIcon"
+                src="../public/img/kukkaLogo.png"
+                alt="kukka 로고"
+                style="width: 167px; height: 22px"
+              />
+            </a>
+          </div>
+          <ul class="flexBox" style="margin: 0 24px; padding: 0">
+            <li><a class="navAnchor headerTag" href="/subscription/">꽃 정기구독</a></li>
+            <li><a class="navAnchor headerTag" href="/shop/flowers/">꽃다발</a></li>
+            <li><a class="navAnchor headerTag" href="/shop/quick/">당일배송</a></li>
+            <li><a class="navAnchor headerTag" href="/class/">플라워클래스</a></li>
+            <li><a class="navAnchor headerTag" href="/item/all/">소품샾</a></li>
+            <li><a class="navAnchor headerTag" href="/event">이벤트</a></li>
           </ul>
-          <div></div>
+          <div>
+            <a href="/">
+              <img class="headerNavIvon" src="../public/img/people.png" alt="마이페이지 로고" />
+            </a>
+            <a href="/cart/?next=/" class="func cart">
+              <img class="headerNavIvon" src="../public/img/basket.png" alt="장바구니 아이콘" />
+            </a>
+          </div>
         </div>
       </nav>
       `;
