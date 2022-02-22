4f430ea9b854306b258998f6bd2a36cc153724bc
gilmujjang <alsanrlf@naver.com>
2022-02-22 22:53:14 +0900

    fix: a tag css gnb 에만 적용

--- src/gnb.js
+++ src/gnb.js
@@ -9,17 +9,17 @@ const initGNB = () => {
       return `
         <nav class="desktopTopNav">
           <ul class="flexBox flexEnd">
-            <li><a class="topNavAnchor" href="/account/login/?next=/">로그인</a></li>
+            <li><a class="topNavAnchor gnbATag" href="/account/login/?next=/">로그인</a></li>
             <li>
-              <a class="topNavAnchor leftBar" href="#">
+              <a class="topNavAnchor leftBar gnbATag" href="#">
                 <span>회원가입</span>
                 <span style="color: #46cfa7">(1000포인트 지급!)</span>
               </a>
             </li>
-            <li><a class="topNavAnchor leftBar" href="/faq">꾸까 고객센터</a></li>
+            <li><a class="topNavAnchor leftBar gnbATag" href="/faq">꾸까 고객센터</a></li>
             <li>
               <a
-                class="topNavAnchor leftBar"
+                class="topNavAnchor leftBar gnbATag"
                 href="https://partners.kukka.kr/?utm_source=kukka&utm_medium=affiliate&utm_campaign=kukka&utm_content=partners"
               >
                 가입제휴
@@ -31,35 +31,35 @@ const initGNB = () => {
           <div class="desktopBottomNav">
             <div class="flexBox spaceBetween" style="width: 100%">
               <div>
-                <a href="/">
+                <a href="/" class="gnbATag">
                   <img class="headerKukkaIcon" src="../public/img/kukkaDeskTopLogo.png" alt="kukka 로고" />
                 </a>
               </div>
               <ul class="flexBox" style="margin: 16px 36px; padding: 0">
                 <li>
-                  <a class="navAnchor" href="#video"><span class="desktopHeaderTag">꽃 정기구독</span></a>
+                  <a class="navAnchor gnbATag" href="#video"><span class="desktopHeaderTag">꽃 정기구독</span></a>
                 </li>
                 <li>
-                  <a class="navAnchor" href="/shop/flowers/"><span class="desktopHeaderTag">꽃다발</span></a>
+                  <a class="navAnchor gnbATag" href="/shop/flowers/"><span class="desktopHeaderTag">꽃다발</span></a>
                 </li>
                 <li>
-                  <a class="navAnchor" href="/shop/quick/"><span class="desktopHeaderTag">당일배송</span></a>
+                  <a class="navAnchor gnbATag" href="/shop/quick/"><span class="desktopHeaderTag">당일배송</span></a>
                 </li>
                 <li>
-                  <a class="navAnchor" href="#image"><span class="desktopHeaderTag">플라워클래스</span></a>
+                  <a class="navAnchor gnbATag" href="#image"><span class="desktopHeaderTag">플라워클래스</span></a>
                 </li>
                 <li>
-                  <a class="navAnchor" href="/item/all/"><span class="desktopHeaderTag">소품샾</span></a>
+                  <a class="navAnchor gnbATag" href="/item/all/"><span class="desktopHeaderTag">소품샾</span></a>
                 </li>
                 <li>
-                  <a class="navAnchor" href="/event"><span class="desktopHeaderTag">이벤트</span></a>
+                  <a class="navAnchor gnbATag" href="/event"><span class="desktopHeaderTag">이벤트</span></a>
                 </li>
               </ul>
               <div style="margin-top: 8px">
-                <a href="/">
+                <a href="/" class="gnbATag">
                   <img class="headerNavIcon" src="../public/img/people.png" alt="마이페이지 로고" />
                 </a>
-                <a href="/cart/?next=/" class="func cart">
+                <a href="/cart/?next=/" class="func cart gnbATag">
                   <img class="headerNavIcon" src="../public/img/basket.png" alt="장바구니 아이콘" />
                 </a>
               </div>
@@ -74,12 +74,12 @@ const initGNB = () => {
       <nav class="mobileTopNav">
         <ul class="flexBox spaceBetween" style="margin: 0">
           <li>
-            <a href="/">
+            <a href="/" class="gnbATag">
               <img class="headerKukkaIcon" src="../public/img/kukkaMobileLogo.png" alt="kukka 로고" />
             </a>
           </li>
           <li>
-            <a href="/cart/?next=/" class="func cart">
+            <a href="/cart/?next=/" class="func cart gnbATag">
               <img class="headerNavIcon" src="../public/img/basket.png" alt="장바구니 아이콘" />
             </a>
           </li>
@@ -87,10 +87,10 @@ const initGNB = () => {
       </nav>
       <nav class="mobileBottomNav">
         <ul class="mobileHeader">
-          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/subscription/">정기구독</a></li>
-          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/shop/flower/">꽃다발</a></li>
-          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/shop/quick/">당일배송</a></li>
-          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag" href="/class/">플라워클래스</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag gnbATag" href="/subscription/">정기구독</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag gnbATag" href="/shop/flower/">꽃다발</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag gnbATag" href="/shop/quick/">당일배송</a></li>
+          <li style="margin-top: 10px"><a class="navAnchor mobileHeaderTag gnbATag" href="/class/">플라워클래스</a></li>
         </ul>
       </nav>
       <div style="height: 1000px"></div>
